4900c01e9f0c13c9986a795efb8370f2578da812
Negin Mansoori <nmansoori@smith.edu>
2023-12-10 21:21:36 -0500

    Restaurant questions

--- Restaurant.java
+++ Restaurant.java
@@ -1,10 +1,15 @@
 import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.Random;
 
 public class Restaurant {
     ArrayList<Table> tables;
-    ArrayList <Food> menu;
+    public ArrayList <Food> menu;
     ArrayList<Customer> customers;
-    String name;
+    public String name;
+    Hashtable <String, Boolean> questions;
+    private Random random;
+    ArrayList <String> questionKeys;
 
     /** Defualt constructor for restaurant */
     public Restaurant (String name){
@@ -12,6 +17,32 @@ public class Restaurant {
         this.tables = new ArrayList<Table>();
         this.menu = new ArrayList<Food>();
         this.customers = new ArrayList<Customer>();
+        this.questions = new Hashtable<String, Boolean>();
+        this.random = new Random();
+        this.questions.put("Do you take debit cards?", random.nextBoolean());
+        this.questions.put("Do you have little spoons for babies?", random.nextBoolean());
+        this.questions.put("Does this restaurant have gender-neutral bathrooms?", random.nextBoolean());
+        this.questions.put("Do you have craft beer?", random.nextBoolean());
+        this.questions.put("Does this restaurant do take out?", random.nextBoolean());
+        this.questions.put("Is smoking inside allowed?", random.nextBoolean());
+        this.questions.put("Are pets allowed in?", random.nextBoolean());
+        this.questions.put("Does Matcha contain caffeine?", true);
+        this.questions.put("Do you have vegetarian dishes?", true);
+        this.questions.put("Do you have vegan dishes?", true);
+        this.questions.put("Are there any drinks without alcohol?", true);
+        this.questionKeys = new ArrayList<>();
+        this.questionKeys.add("Do you take debit cards?");
+        this.questionKeys.add("Do you have little spoons for babies?");
+        this.questionKeys.add("Does this restaurant have gender-neutral bathrooms?");
+        this.questionKeys.add("Do you have craft beer?");
+        this.questionKeys.add("Does this restaurant do take out?");
+        this.questionKeys.add("Is smoking inside allowed?");
+        this.questionKeys.add("Are pets allowed in?");
+        this.questionKeys.add("Does Matcha contain caffeine?");
+        this.questionKeys.add("Do you have vegetarian dishes?");
+        this.questionKeys.add("Do you have vegan dishes?");
+        this.questionKeys.add("Are there any drinks without alcohol?");
+        
     }
      /**
       * Overloaded constructor with the number of tables
