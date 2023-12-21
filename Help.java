af5fc5e3a3fae8c89af80bb73f5004c29f98f901
rehabmalik <144557438+rehabmalik@users.noreply.github.com>
2023-12-20 22:46:42 -0600

    Update Help.java

--- Help.java
+++ Help.java
@@ -21,12 +21,19 @@ public class Help{
      * Allows the player to choose to get a list of available commands they can use or access the game rules again
      */
     public static void help(){
-        System.out.println("\nWhat do you need help with?\n" + "A. Available Commands\n" + "B. Game Rules\n");
+        Game.printBorder(25);
+        System.out.println("What do you need help with?\n" + "A. Available Commands\n" + "B. Game Rules");
         String scan1 = scanner.nextLine();
+        while (!scan1.equalsIgnoreCase("A") && !scan1.equalsIgnoreCase("B")) {
+            System.out.println("Choose one of the given options.");
+            scan1 = scanner.nextLine();
+        }
         if (scan1.equalsIgnoreCase("A")){
-            System.out.println("This is a list of all the commands you can use.\n\n---");
+            System.out.println("This is a list of all the commands you can use.\n-----");
             System.out.println("welcome\ngreet\nenter\nserve\nhelp");
-            System.out.println("---\n");
+            System.out.println("Press enter to move to the next line of dialogue.");
+            System.out.println("-----");
+            System.out.println("Try using one of the commands now!");
         }
 
         else if (scan1.equalsIgnoreCase("B")){
@@ -36,17 +43,16 @@ public class Help{
             System.out.println("- *greet* your table once they've all sat down and take their order.");
             System.out.println("- *enter* their order so it gets sent to the kitchen.");
             System.out.println("- *serve* their food.\n");
+            System.out.println("- Press enter to move to the next line of dialogue.");
             System.out.println("- If customers have any questions, be sure to answer them by picking the correct answer that you will be given beforehand.");
-            System.out.println("- Don't be afraid to ask for help, but make sure you answer customers quickly. We wouldn't want to keep anyone waiting!\n----------\n\n");
+            System.out.println("- Don't be afraid to ask for help, but make sure you answer customers quickly. We wouldn't want to keep anyone waiting!\n----------");
+            System.out.println("Try using one of the commands now!");
         }
 
-        else {
-            help();
-        }
+        Game.printBorder(30);
     }
 
     public static void main(String[] args) {
-        Help gameHelp = new Help();
-        gameHelp.help();
+        Help.help();
     }
 }
